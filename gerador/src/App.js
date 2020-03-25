78da250d9ec2461f2d562c5cf182637a0de0b5bc
AdrianoSilvaJesus <adrianosilvaoliveiradejesus@outlook.com>
2020-03-24 22:28:03 -0300

    Adicionado Botões e estilos

--- gerador/src/App.js
+++ gerador/src/App.js
@@ -4,9 +4,19 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello,World !</h1>
+      <div className="botoes">
+        <Botao texto="Layout 1"/>
+        <Botao texto="Layout 2"/>
+        <Botao texto="Layout 3"/>
+      </div>
     </div>
   );
-}
+};
+
+const Botao = props => {
+  return(
+    <button className="botao">{props.texto}</button>
+  );
+};
 
 export default App;
